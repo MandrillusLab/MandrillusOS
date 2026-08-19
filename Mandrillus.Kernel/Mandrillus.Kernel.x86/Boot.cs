184c1157e0a4928981a2b3cf41c9fd8893af7906
Leandro Vieira Santos <leandrown@outlook.com>
2026-08-19 17:26:57 -0300

    Add project files.

---
+++ Mandrillus.Kernel/Mandrillus.Kernel.x86/Boot.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using Mosa.Kernel.BareMetal;
+
+namespace Mandrillus.Kernel.x86;
+
+public static class Boot
+{
+	public static void Main()
+	{
+		Debug.WriteLine("Boot::Main()");
+		Debug.WriteLine("MOSA x86 Kernel");
+
+		Program.EntryPoint();
+	}
+}
