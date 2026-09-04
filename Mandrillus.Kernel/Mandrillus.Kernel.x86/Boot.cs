24c81a0b418683e2f682a5c245d503302e5545ce
Leandro Vieira Santos <leandrown@outlook.com>
2026-09-04 12:12:08 -0300

    Update copyright and license in Boot.cs
    Replaced old copyright lines with "Leandro Vieira / Mandrillus Systems" for 2026. Clarified license reference to MIT License and LICENSE file.

--- Mandrillus.Kernel/Mandrillus.Kernel.x86/Boot.cs
+++ Mandrillus.Kernel/Mandrillus.Kernel.x86/Boot.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
-// Copyright (c) Mandrillus OS. Licensed under the MIT License.
+// Copyright (c) 2026 Leandro Vieira / Mandrillus Systems
+// Licensed under the MIT License. See LICENSE file in the project root.
 using Mosa.Kernel.BareMetal;
 
 namespace Mandrillus.Kernel.x86;
